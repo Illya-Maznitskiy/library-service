9239b8ab78314712301353fa09d5d7ffc457ef13
Illya Maznitskiy <comercaleuros@gmail.com>
2024-10-31 19:17:19 +0200

    Update constraints for borrow_date, expected_return_date, and actual_return_date

--- borrowings/models.py
+++ borrowings/models.py
@@ -12,18 +12,5 @@ class Borrowing(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    def clean(self):
-        if self.expected_return_date <= self.borrow_date:
-            raise ValidationError(
-                "Expected return date must be after borrow date."
-            )
-        if (
-            self.actual_return_date
-            and self.actual_return_date < self.borrow_date
-        ):
-            raise ValidationError(
-                "Actual return date cannot be before the borrow date."
-            )
-
     def __str__(self):
         return f"Borrowed on {self.borrow_date}"
--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -16,3 +16,20 @@ class BorrowingSerializer(serializers.ModelSerializer):
             "expected_return_date",
             "actual_return_date",
         ]
+
+    def validate(self, attrs):
+        borrow_date = attrs.get("borrow_date")
+        expected_return_date = attrs.get("expected_return_date")
+        actual_return_date = attrs.get("actual_return_date")
+
+        if expected_return_date <= borrow_date:
+            raise serializers.ValidationError(
+                "Expected return date must be after borrow date."
+            )
+
+        if actual_return_date and actual_return_date < borrow_date:
+            raise serializers.ValidationError(
+                "Actual return date cannot be before the borrow date."
+            )
+
+        return attrs
