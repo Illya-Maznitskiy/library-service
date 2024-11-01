f2b2c04e92639a42fed093ed94ae07c91b082cee
Illya Maznitskiy <comercaleuros@gmail.com>
2024-11-01 11:23:37 +0200

    Fix indentation in Borrowings urls.py

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
-
 from borrowings.views import BorrowingViewSet
-
 
 router = routers.DefaultRouter()
 router.register("", BorrowingViewSet)
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("", include(router.urls)),
+]
